7afaf71ccbc91afd5c5cb5309d1b5dc1ee428b52
viktoria <vika.ibraimova85@bk.ru>
2020-03-26 23:02:53 +0300

    add Version 1,3 Update Version 3

---
+++ Version 3/FinalProject — копия/src/main/java/pages/BasePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import general.DriverManager;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BasePage {
+
+}
---
+++ Version 3/FinalProject — копия/src/main/java/pages/SettingsPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+
+import java.net.MalformedURLException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SettingsPage extends BasePage {
+}
